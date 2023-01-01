9ed4ed5b81041f797933ce423b0be2fa41fa74f2
Lucie7798 <lucie.guinebert@laposte.net>
2023-01-01 18:22:49 +0100

    animer Mario

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// On va chercher tous les éléments sur lesquels on intervient
+let mario = document.querySelector("#mario")
+let solHaut = document.querySelector("#sol-haut")
+let solBas = document.querySelector("#sol-bas")
+let paysage = document.querySelector("#paysage")
+let ciel = document.querySelector("#body")
+
+// On conserve le "décalage" des éléments
+let decalage =0
+
+window.onload = () => {
+    document.addEventListener("keydown", marcher)
+    document.addEventListener("keyup", stopper)
+}
+
+/**
+ * Cette fonction fait marcher Mario
+ */
+function marcher(event){
+    switch(event.key){
+        case "ArrowLeft":
+            // On anime Mario
+            mario.classList.add("mario-gauche")
+            mario.classList.remove("mario-droite")
+            parallaxe()
+            decalage++
+            break
+        case "ArrowRight":
+            mario.classList.add("mario-droite")
+            mario.classList.remove("mario-gauche")
+            parallaxe()
+            decalage--
+            break
+    }
+}
+
+/**
+ * Cette fonction arrête Mario
+ */
+function stopper(event){
+    mario.classList.remove("mario-gauche")
+    mario.classList.remove("mario-droite")
+}
